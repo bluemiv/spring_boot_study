befe93deda1f3e05bf3c4c3fb8646a01151bcb41
TaehongKim <public.devhong@gmail.com>
2020-11-12 17:28:02 +0900

    Add restaurant domain

--- FoodFighter/fofi-api/src/main/java/com/example/FoodFighter/domain/Restaurant.java
+++ FoodFighter/fofi-api/src/main/java/com/example/FoodFighter/domain/Restaurant.java
@@ -2,27 +2,38 @@ package com.example.FoodFighter.domain;
 
 public class Restaurant {
 
-    private String name;
-    private String address;
+  private Long id;
+  private String name;
+  private String address;
 
-    public Restaurant(String name) {
-        this.name = name;
-    }
+  public Restaurant(String name) {
+    this.name = name;
+  }
 
-    public Restaurant(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
+  public Restaurant(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public Restaurant(Long id, String name, String address) {
+    this.name = name;
+    this.address = address;
+    this.id = id;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getInformation() {
-        return name + " in " + address;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getInformation() {
+    return name + " in " + address;
+  }
 }
---
+++ FoodFighter/fofi-api/src/main/java/com/example/FoodFighter/interfaces/RestaurantController.java
@@ -0,0 +1,21 @@
+package com.example.FoodFighter.interfaces;
+
+import com.example.FoodFighter.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+  @GetMapping("/restaurant")
+  public List<Restaurant> list() {
+    List<Restaurant> restaurants = new ArrayList<>();
+
+    Restaurant restaurant = new Restaurant(1004L, "My Food House", "Seoul");
+    restaurants.add(restaurant);
+    return restaurants;
+  }
+}
